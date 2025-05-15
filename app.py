0eb0c70b474414d0ab40e002fb69d770cd14aee9
Sumit yadav <158730494+sumityadav7090@users.noreply.github.com>
2025-05-16 00:18:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,11 @@
-from flask import Flask, render_template, request
-import hashlib
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
-    text = ""
-    sha224 = None
-    sha256 = None
-    hash_type = "both"
-
-    if request.method == "POST":
-        text = request.form["text"]
-        hash_type = request.form["hash_type"]
-
-        if hash_type == "sha224" or hash_type == "both":
-            sha224 = hashlib.sha224(text.encode()).hexdigest()
-
-        if hash_type == "sha256" or hash_type == "both":
-            sha256 = hashlib.sha256(text.encode()).hexdigest()
-
-    return render_template("index.html", text=text, sha224=sha224, sha256=sha256, hash_type=hash_type)
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    
+    app.run( debug=True)
